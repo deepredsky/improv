20460ac8b4836e70a7c49073105187f79b2842ba
Rajesh Sharma <rajesh@surf.net.np>
2017-07-22 17:29:21 +0200

    Spit files

--- app/Main.hs
+++ app/Main.hs
@@ -1,51 +1,8 @@
 {-# LANGUAGE OverloadedStrings #-}
 
-import Data.Text (Text)
-import Data.List (isInfixOf)
-import qualified Data.Text as T
-import qualified Data.Text.Encoding as TE
-import qualified Data.ByteString.Char8 as B
-import qualified Data.ByteString.Lazy.Char8 as LB
-import Data.Conduit
-import qualified Data.Conduit.Combinators as CC
-import qualified Data.Conduit.List as CL
-import qualified Data.Conduit.Binary as CB
-import Control.Applicative
-import Data.Char
-import Data.Maybe
-import Control.Monad
-import Control.Monad.IO.Class
-import System.Directory (doesFileExist)
-import System.IO
 import System.Environment (getArgs)
-import Text.Regex.PCRE
+import Grep (processFiles)
 
-parseLines pattern = CL.filter f
-  where
-    f :: (B.ByteString, B.ByteString) -> Bool
-    f (_, x) = x =~ (pattern :: String)
-
-printMatch = CL.mapM_ (\(id, _) -> liftIO $ putStrLn $ show id)
-
-
-processFiles :: String -> [String] -> IO ()
-processFiles pattern = mapM_ (processFile pattern)
-
-processFile :: String -> String -> IO ()
-processFile _ [] = return ()
-processFile pattern file = do
-    exists <- doesFileExist file
-    if not exists
-        then putStrLn $ file ++ ": file does not exists"
-        else do
-            let parseConduit = CL.map $ B.span (':' /=)
-
-            runConduitRes $
-              CB.sourceFile file
-              .| CB.lines
-              .| parseConduit
-              .| parseLines pattern
-              .| printMatch
 main = do
     args <- getArgs
     processFiles (head args) (tail args)
