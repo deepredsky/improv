9f3b3dde814b255c72180320aaf28e6e4da05748
Rajesh Sharma <rajesh@surf.net.np>
2017-07-22 17:56:29 +0200

    Remove unused imports

--- src/Grep.hs
+++ src/Grep.hs
@@ -2,33 +2,24 @@
 
 module Grep (processFiles) where
 
-import Data.Text (Text)
-import Data.List (isInfixOf)
-import qualified Data.Text as T
-import qualified Data.Text.Encoding as TE
 import qualified Data.ByteString.Char8 as B
 import qualified Data.ByteString.Lazy.Char8 as LB
-import Data.Conduit
+
+import Data.Conduit((.|), runConduitRes)
 import qualified Data.Conduit.Combinators as CC
 import qualified Data.Conduit.List as CL
 import qualified Data.Conduit.Binary as CB
-import Control.Applicative
-import Data.Char
-import Data.Maybe
-import Control.Monad
+
 import Control.Monad.IO.Class
 import System.Directory (doesFileExist)
-import System.IO
 import Text.Regex.PCRE
-
 
 parseLines pattern = CL.filter f
   where
     f :: (B.ByteString, B.ByteString) -> Bool
     f (_, x) = x =~ (pattern :: String)
 
-printMatch = CL.mapM_ (\(id, _) -> liftIO $ putStrLn $ show id)
-
+printMatch = CL.mapM_ (\(lineId, _) -> liftIO $ putStrLn $ show lineId)
 
 processFiles :: String -> [String] -> IO ()
 processFiles pattern = mapM_ (processFile pattern)
